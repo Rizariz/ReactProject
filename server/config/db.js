1783f83f5f42a663824b7bf47160a8918ba88dcf
Riza-ui <86155051+Rizariz@users.noreply.github.com>
2022-05-19 13:42:09 +1000

    Added my project

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async()=>{
+    try {
+        console.log('Test');
+        const connection = await mongoose.connect(process.env.MONGO_URI,{
+            useCreateIndex:true,
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
+        console.log(`MongoDB Connected:${connection.connection.host}`);
+
+    } catch (error) {   
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
